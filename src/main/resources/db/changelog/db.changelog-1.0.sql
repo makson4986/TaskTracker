f52431c5d25bc1000f7f88db49c1174b40380d18
makson <maksbatkaev06@gmail.com>
2025-08-20 14:27:31 +0300

    create: database

---
+++ src/main/resources/db/changelog/db.changelog-1.0.sql
@@ -0,0 +1,24 @@
+--liquibase formatted sql
+
+--changeset makson:1
+CREATE TABLE IF NOT EXISTS users
+(
+    id       SERIAL PRIMARY KEY,
+    email    VARCHAR(16) UNIQUE NOT NULL,
+    password VARCHAR(512)       NOT NULL
+);
+--rollback DROP TABLE users;
+
+--changeset makson:2
+CREATE TABLE IF NOT EXISTS tasks
+(
+    id     SERIAL PRIMARY KEY,
+    title  VARCHAR(256) NOT NULL,
+    text   TEXT         NOT NULL,
+    owner  INT          NOT NULL,
+    status VARCHAR(32)  NOT NULL DEFAULT 'NOT_DONE',
+    performedAt   TIMESTAMP,
+    FOREIGN KEY (owner) REFERENCES users (id)
+);
+--rollback DROP TABLE tasks;
+
